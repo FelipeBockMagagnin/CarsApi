d0544752ad6c446fdaa4fd86c169d5ae7f94c075
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-12-07 12:07:21 -0300

    Teste

--- src/main/java/com/ifrs/carrocaro/CarroCaro/controllers/CarroController.java
+++ src/main/java/com/ifrs/carrocaro/CarroCaro/controllers/CarroController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import javax.validation.Valid;
+import com.ifrs.carrocaro.CarroCaro.repositories.*;
 
 @Controller
 public class CarroController {
@@ -15,35 +16,42 @@ public class CarroController {
 
   @CrossOrigin
   @GetMapping("/Carro")
-  public List<Carro> all() {
-     return Carro.getAllCarros();
+  public ResponseEntity<String> get()
+  {
+    return ResponseEntity.ok("[{'carId':1,'nome':'Vectra','datacompra':'2019-10-02','placa':'itk-2098','preco':1000.0,'formacompra':'dinheiro','datafabricacao':'2018-10-10','foto':'','descricao':'carro bonito'},{'carId':2,'nome':'Vectra','datacompra':'2019-10-02','placa':'itk-2098','preco':1000.0,'formacompra':'dinheiro','datafabricacao':'2018-10-10','foto':'','descricao':'carro bonito'},{'carId':3,'nome':'Carro teste','datacompra':'2019-10-02','placa':'itk-2098','preco':3000.0,'formacompra':'dinheiro','datafabricacao':'2018-10-10','foto':'','descricao':'carro bonito'}]");
   }
 
-  @CrossOrigin
-  @PostMapping("/Carro")
-  public ResponseEntity<Carro> createCarro (@Valid @RequestBody Carro Carro) {
-      return ResponseEntity.ok(Carro.saveCarro(Carro));
-  }
+  // @CrossOrigin
+  // @GetMapping('/Carro')
+  // public List<Carro> all() {
+  //    return Carro.getAllCarros();
+  // }
 
-  @CrossOrigin
-  @PutMapping("/Carro/{id}")
-  public ResponseEntity<Carro> updateCarro(@Valid @RequestBody Carro Carro,
-    @PathVariable(value= "id") Long id) {
-        return ResponseEntity.ok(Carro.updateCarro(Carro, id));
-    }
+  // @CrossOrigin
+  // @PostMapping('/Carro')
+  // public ResponseEntity<Carro> createCarro (@Valid @RequestBody Carro Carro) {
+  //     return ResponseEntity.ok(Carro.saveCarro(Carro));
+  // }
 
-  @CrossOrigin
-  @DeleteMapping("/Carro/{id}")
-  public ResponseEntity<?> deleteMemeber(@PathVariable Long id) {
-    Map<String,String> response = new HashMap<String,String>();
-    if(Carro.deleteCarro(id)) {
-      response.put("status", "success");
-      response.put("message", "Carro deleted successfully");
-       return ResponseEntity.ok(response);
-    } else {
-      response.put("status", "error");
-      response.put("message", "Somthing went wrong when delete the Carro");
-      return ResponseEntity.status(500).body(response);
-    }
-  }
+  // @CrossOrigin
+  // @PutMapping('/Carro/{id}')
+  // public ResponseEntity<Carro> updateCarro(@Valid @RequestBody Carro Carro,
+  //   @PathVariable(value= 'id') Long id) {
+  //       return ResponseEntity.ok(Carro.updateCarro(Carro, id));
+  //   }
+
+  // @CrossOrigin
+  // @DeleteMapping('/Carro/{id}')
+  // public ResponseEntity<?> deleteMemeber(@PathVariable Long id) {
+  //   Map<String,String> response = new HashMap<String,String>();
+  //   if(Carro.deleteCarro(id)) {
+  //     response.put('status', 'success');
+  //     response.put('message', 'Carro deleted successfully');
+  //      return ResponseEntity.ok(response);
+  //   } else {
+  //     response.put('status', 'error');
+  //     response.put('message', 'Somthing went wrong when delete the Carro');
+  //     return ResponseEntity.status(500).body(response);
+  //   }
+  // }
 }
--- src/main/java/com/ifrs/carrocaro/CarroCaro/controllers/EventoController.java
+++ src/main/java/com/ifrs/carrocaro/CarroCaro/controllers/EventoController.java
@@ -13,36 +13,43 @@ public class EventoController {
   private Evento Evento;
 
   @CrossOrigin
-  @GetMapping("/Evento")
-  public List<Evento> all() {
-     return Evento.getAllEventos();
+  @GetMapping("/evento")
+  public ResponseEntity<String> get()
+  {
+    return ResponseEntity.ok("[{'eventId':1,'titulo':'tag','data':'data','tag':'evento legal','valor':1.0,'descricao':'titulo','id_carro':100},{'eventId':2,'titulo':'tag','data':'data','tag':'evento legal','valor':1.0,'descricao':'titulo','id_carro':100},{'eventId':3,'titulo':'tag','data':'data','tag':'evento legal','valor':1.0,'descricao':'titulo','id_carro':100},{'eventId':5,'titulo':'tag','data':'data','tag':'evento legal','valor':1.0,'descricao':'titulo','id_carro':100},{'eventId':6,'titulo':'tag','data':'data','tag':'evento legal','valor':1.0,'descricao':'titulo','id_carro':100},{'eventId':7,'titulo':'tag','data':'data','tag':'evento legal','valor':1.0,'descricao':'titulo','id_carro':100},{'eventId':8,'titulo':'tag','data':'data','tag':'evento legal','valor':1300.0,'descricao':'titulo','id_carro':100},{'eventId':9,'titulo':'titulo teste','data':'12-03-2019','tag':'evento legal','valor':200.0,'descricao':'titulo','id_carro':1},{'eventId':10,'titulo':'titulo teste','data':'12-03-2019','tag':'evento legal','valor':200.0,'descricao':'titulo','id_carro':1},{'eventId':11,'titulo':'titulo teste','data':'12-03-2019','tag':'evento legal','valor':200.0,'descricao':'titulo','id_carro':1},{'eventId':12,'titulo':'titulo teste','data':'12-03-2019','tag':'evento legal','valor':20032.0,'descricao':'titulo','id_carro':1}]");
   }
 
-  @CrossOrigin
-  @PostMapping("/Evento")
-  public ResponseEntity<Evento> createEvento (@Valid @RequestBody Evento Evento) {
-      return ResponseEntity.ok(Evento.saveEvento(Evento));
-  }
+  // @CrossOrigin
+  // @GetMapping('/Evento')
+  // public List<Evento> all() {
+  //    return Evento.getAllEventos();
+  // }
 
-  @CrossOrigin
-  @PutMapping("/Evento/{id}")
-  public ResponseEntity<Evento> updateEvento(@Valid @RequestBody Evento Evento,
-    @PathVariable(value= "id") Long id) {
-        return ResponseEntity.ok(Evento.updateEvento(Evento, id));
-    }
+  // @CrossOrigin
+  // @PostMapping('/Evento')
+  // public ResponseEntity<Evento> createEvento (@Valid @RequestBody Evento Evento) {
+  //     return ResponseEntity.ok(Evento.saveEvento(Evento));
+  // }
 
-  @CrossOrigin
-  @DeleteMapping("/Evento/{id}")
-  public ResponseEntity<?> deleteMemeber(@PathVariable Long id) {
-    Map<String,String> response = new HashMap<String,String>();
-    if(Evento.deleteEvento(id)) {
-      response.put("status", "success");
-      response.put("message", "Evento deleted successfully");
-       return ResponseEntity.ok(response);
-    } else {
-      response.put("status", "error");
-      response.put("message", "Somthing went wrong when delete the Evento");
-      return ResponseEntity.status(500).body(response);
-    }
-  }
+  // @CrossOrigin
+  // @PutMapping('/Evento/{id}')
+  // public ResponseEntity<Evento> updateEvento(@Valid @RequestBody Evento Evento,
+  //   @PathVariable(value= 'id') Long id) {
+  //       return ResponseEntity.ok(Evento.updateEvento(Evento, id));
+  //   }
+
+  // @CrossOrigin
+  // @DeleteMapping('/Evento/{id}')
+  // public ResponseEntity<?> deleteMemeber(@PathVariable Long id) {
+  //   Map<String,String> response = new HashMap<String,String>();
+  //   if(Evento.deleteEvento(id)) {
+  //     response.put('status', 'success');
+  //     response.put('message', 'Evento deleted successfully');
+  //      return ResponseEntity.ok(response);
+  //   } else {
+  //     response.put('status', 'error');
+  //     response.put('message', 'Somthing went wrong when delete the Evento');
+  //     return ResponseEntity.status(500).body(response);
+  //   }
+  // }
 }
