d066243be75fa29514d6435ee052acaec5b67510
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-12-08 17:55:56 -0300

    teste

--- src/main/java/com/ifrs/carrocaro/CarroCaro/repositories/EventoRepository.java
+++ src/main/java/com/ifrs/carrocaro/CarroCaro/repositories/EventoRepository.java
@@ -5,7 +5,7 @@ import com.ifrs.carrocaro.CarroCaro.models.Evento;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface EventoRepository extends CrudRepository<Evento, String> {
+public interface EventoRepository extends CrudRepository<Evento, Long> {
     //List<Evento> getEventos();
     Evento findByid(long id);
 }
