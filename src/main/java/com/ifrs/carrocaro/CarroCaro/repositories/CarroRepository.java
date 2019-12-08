52fa6ab08a4f7477c7c537948f026a56eb252283
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-12-08 19:48:23 -0300

    a

--- src/main/java/com/ifrs/carrocaro/CarroCaro/repositories/CarroRepository.java
+++ src/main/java/com/ifrs/carrocaro/CarroCaro/repositories/CarroRepository.java
@@ -14,12 +14,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CarroRepository extends JpaRepository<Carro, Long> {
     //List<Carro> getCarros();
-    @Query("SELECT o FROM ConcreteOperation o WHERE o.beginTimestamp BETWEEN :from AND :to AND o.status = :status AND o.terminal.deviceId = :deviceId AND o.trainingMode = :trainingMode")
-Collection<Carro> findOperations(
-    @Param("from") @Temporal(TemporalType.TIMESTAMP) Date startDay,
-    @Param("to") @Temporal(TemporalType.TIMESTAMP) Date endDay,
-    @Param("status") OperationStatus status,
-    @Param("deviceId") String deviceId,
-    @Param("trainingMode") boolean trainingMode
-);
+//     @Query("SELECT o FROM ConcreteOperation o WHERE o.beginTimestamp BETWEEN :from AND :to AND o.status = :status AND o.terminal.deviceId = :deviceId AND o.trainingMode = :trainingMode")
+// Collection<Carro> findOperations(
+//     @Param("from") @Temporal(TemporalType.TIMESTAMP) Date startDay,
+//     @Param("to") @Temporal(TemporalType.TIMESTAMP) Date endDay,
+//     @Param("status") OperationStatus status,
+//     @Param("deviceId") String deviceId,
+//     @Param("trainingMode") boolean trainingMode
+// );
 }
