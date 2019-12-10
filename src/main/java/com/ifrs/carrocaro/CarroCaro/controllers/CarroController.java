078ea11385cf2cf26a9e9490ee85a7e503d8fc90
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-12-09 21:24:46 -0300

    delete carro

--- src/main/java/com/ifrs/carrocaro/CarroCaro/controllers/CarroController.java
+++ src/main/java/com/ifrs/carrocaro/CarroCaro/controllers/CarroController.java
@@ -14,39 +14,44 @@ import com.ifrs.carrocaro.CarroCaro.repositories.*;
 @Controller
 public class CarroController {
 
-  @Autowired
-  private CarroRepository cr;
+    @Autowired
+    private CarroRepository cr;
 
-  @CrossOrigin
-  @GetMapping("/carro")
-  public ResponseEntity<List<Carro>> getAll() {
-    return ResponseEntity.ok(cr.findAll());
-  }
+    @Autowired
+    private EventoRepository er;
 
-  @CrossOrigin
-  @PostMapping("/carro")
-  public ResponseEntity<Carro> post(@Valid @RequestBody Carro evento) {
-    return ResponseEntity.ok(cr.save(evento));
-  }
+    @CrossOrigin
+    @GetMapping("/carro")
+    public ResponseEntity<List<Carro>> getAll() {
+        return ResponseEntity.ok(cr.findAll());
+    }
 
-  @CrossOrigin
-  @PutMapping("/Carro/{id}")
-  public ResponseEntity<Carro> updateCarro(@Valid @RequestBody Carro Carro, @PathVariable(value = "id") Long id) {
-    return ResponseEntity.ok(cr.save(Carro));
-  }
+    @CrossOrigin
+    @PostMapping("/carro")
+    public ResponseEntity<Carro> post(@Valid @RequestBody Carro evento) {
+        return ResponseEntity.ok(cr.save(evento));
+    }
 
-  // @CrossOrigin
-  // @DeleteMapping('/Carro/{id}')
-  // public ResponseEntity<?> deleteMemeber(@PathVariable Long id) {
-  // Map<String,String> response = new HashMap<String,String>();
-  // if(Carro.deleteCarro(id)) {
-  // response.put('status', 'success');
-  // response.put('message', 'Carro deleted successfully');
-  // return ResponseEntity.ok(response);
-  // } else {
-  // response.put('status', 'error');
-  // response.put('message', 'Somthing went wrong when delete the Carro');
-  // return ResponseEntity.status(500).body(response);
-  // }
-  // }
+    // @CrossOrigin
+    // @PutMapping("/Carro/{id}")
+    // public ResponseEntity<Carro> updateCarro(@Valid @RequestBody Carro Carro,
+    // @PathVariable(value = "id") Long id) {
+    // return ResponseEntity.ok(cr.save(Carro));
+    // }
+
+    @CrossOrigin
+    @DeleteMapping("/carro/{id}")
+    public ResponseEntity<Map<String, String>> deleteCarro(@PathVariable Long id) {
+        Map<String, String> response = new HashMap<String, String>();
+        try {
+            cr.deleteById(id);
+            response.put("status", "success");
+            response.put("message", "Carro deleted successfully");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("status", "error");
+            response.put("message", "Something went wrong when delete the Carro");
+            return ResponseEntity.status(500).body(response);
+        }
+    }
 }
